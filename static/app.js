4aaedb6cfa5d9f4aebf154931276d464215b1b8a
root <root@ubuntu-server.gateway.2wire.net>
2011-10-30 19:15:19 -0700

    New Commit

---
+++ static/app.js
@@ -0,0 +1,27 @@
+function urlencode (str) {
+	str = (str + '').toString();
+    return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+    replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+}
+		
+		
+		$(document).keypress(function(e) {
+    if(e.keyCode == 13) {
+doSubmit();
+    }
+});
+		  var socket = io.connect("%HOSTURI%/google");
+		  var loadone = false;
+		  var i = 0;
+		  var aspi = "";
+		  socket.on('resulted', function(data){
+			  $('#bgbox').append('<span style="display:none;" id="' + i + '">' + data.query + '</span><br />');
+			  aspi = "#" + i;
+			  $(aspi).fadeIn("slow");
+			i++;
+		  });
+		 
+	function doSubmit(){
+		socket.emit('search', {query: $("#searchbar").val()});
+		window.open('http://www.google.com/search?q=' + urlencode($("#searchbar").val()), '_blank');
+	}
